49d682b693a96b24f77cf026da7c4d2f1cad8a8f
sameermansuri09898 <sameermansuri09898@gmail.com>
2026-06-22 08:20:54 +0530

    update

--- sellerdash/serializer.py
+++ sellerdash/serializer.py
@@ -23,35 +23,35 @@ class sellerserializer(serializers.ModelSerializer):
     if not bank_account or not bank_ifsc_code or not bank_name or not bank_branch or not bank_account_holder_name:
       raise serializers.ValidationError("Bank Account Details are Required")
 
-      if not bank_account.isdigit():
+    if not bank_account.isdigit():
         raise serializers.ValidationError("Bank Account Number Must Be in Numbers")
 
-      if not bank_ifsc_code.isalnum():
+    if not bank_ifsc_code.isalnum():
         raise serializers.ValidationError("Bank IFSC Code Must Be Alphanumeric")    
 
-      if not bank_name.isalpha():
+    if not bank_name.isalpha():
         raise serializers.ValidationError("Bank Name Must Be In Alphabets")    
 
-      if not bank_branch.isalpha():
+    if not bank_branch.isalpha():
         raise serializers.ValidationError("Bank Branch Must Be In Alphabets")    
 
-      if not bank_account_holder_name.isalpha():
+    if not bank_account_holder_name.isalpha():
         raise serializers.ValidationError("Bank Account Holder Name Must Be In Alphabets")    
 
 
-      if not pan or not gst or not aadhaar or not shop_phone:
+    if not pan or not gst or not aadhaar or not shop_phone:
         raise serializers.ValidationError("Pan, GST, Aadhaar and Shop Phone are Required")
 
-      if not pan.isalnum():
+    if not pan.isalnum():
         raise serializers.ValidationError("Pan Number Must Be Alphanumeric")    
 
-      if not gst.isalnum():
+    if not gst.isalnum():
         raise serializers.ValidationError("GST Number Must Be Alphanumeric")    
 
-      if not aadhaar.isdigit():
+    if not aadhaar.isdigit():
         raise serializers.ValidationError("Aadhaar Number Must Be In Numbers")    
 
-      if not shop_phone.isdigit():
+    if not shop_phone.isdigit():
         raise serializers.ValidationError("Shop Phone Number Must Be In Numbers")  
 
 
